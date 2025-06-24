a87e96d42d5c164e5f5fe0236ebc78069fe399b1
RaveenDelath <raveendlth@gmai.com>
2025-06-24 05:17:04 +0100

    Create first sample end point that will return the data when the client request

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ app = Flask(__name__)
 
 projects = [
     {
-        
+        "name": "My Store",
+        "items": [
+            {
+                "name": "Chair",
+                "price": 15.99
+            }
+        ]
     }
 ]
